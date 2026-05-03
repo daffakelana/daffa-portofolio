4177c2cb07feb4f69010b40410b1c5ae59365ee4
daffakelana <daffadarakelana@student.upi.edu>
2026-05-03 17:46:15 +0700

    add cms and frontend integration

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,6 +3,9 @@
 import { ThemeProvider } from 'next-themes'
 
 export function Providers({ children }: { children: React.ReactNode }) {
+
+  console.log();
+  
   return (
     <ThemeProvider attribute="class" disableTransitionOnChange>
       {children}
